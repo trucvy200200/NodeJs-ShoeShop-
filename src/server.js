6f6176abfb1a7e50aac703f6798b70e3b4a94cc7
trucvy200200 <win 10@DESKTOP-I1H1J2K>
2022-10-29 20:37:52 +0700

    commit

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
-const express = require('express')
+import express from "express";
+import configViewEngine from "./config/ViewEngine";
 const app = express()
 const port = 3000
 
+configViewEngine(app);
+
+//set url pattern
 app.get('/', (req, res) => {
-    res.send('Hello World! voi Vy')
+    res.render('index.ejs');
 })
 
 app.listen(port, () => {
